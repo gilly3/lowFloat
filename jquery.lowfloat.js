9b1732b23d42eb386c0c525e759e5ca262274adb
Ivan Gilchrist <github@jumpingfishes.com>
2013-11-07 13:40:34 -0800

    Create jquery.lowfloat.js

---
+++ jquery.lowfloat.js
@@ -0,0 +1,31 @@
+$.fn.lowFloat = function(options){
+    var opts = $.extend({}, $.fn.lowFloat.defaultOptions, options);
+    this.each(function(){
+        var container = opts.closest ? $(this).closest(opts.closest) : $(this).parent();
+        container.prepend(this);
+        $(this).css({ float: opts.float, clear: opts.float });
+        $("<div>").css({ width: 0, float: opts.float }).insertBefore(this);
+    });
+    var floaters = this;
+    $(window).on("resize", function(){
+        floaters.each(function(){
+            var floater = $(this);
+            var spacer = floater.prev();
+            var container = floater.parent();
+            var lastContent = container.children().last();
+            var floaterHeight = this.offsetHeight;
+            var h = Math.max(0, container.height() - floaterHeight);
+            spacer.height(h);
+            while (h > 0 && floater.offset().top + floaterHeight > lastContent.offset().top + lastContent.height()) {
+                spacer.height(--h);
+            }
+            while (floater.offset().top + floaterHeight < lastContent.offset().top + lastContent.height()) {
+                spacer.height(++h);
+            }
+        });
+    }).trigger("resize");
+};
+$.fn.lowFloat.defaultOptions = {
+    closest: null,
+    float: "right"
+};
