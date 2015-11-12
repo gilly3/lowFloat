04237f58955c04abd723e129788c30c320393713
Ivan Gilchrist <igilchrist@id-info.com>
2015-11-12 15:08:34 -0800

    Allow disabling lowFloat via CSS

--- jquery.lowfloat.js
+++ jquery.lowfloat.js
@@ -19,24 +19,32 @@
         this.each(function () {
             var container = $(this).parent().closest(opts.container);
             container.prepend(this);
-            $(this).css({ float: opts.float, clear: opts.float });
-            $("<div>").css({ width: 0, float: opts.float }).insertBefore(this);
+            $(this).css({
+                float: opts.float,
+                clear: opts.float
+            });
+            $("<div>").addClass("lowFloat-spacer").css({
+                width: 0,
+                float: opts.float
+            }).insertBefore(this);
         });
         var floaters = this;
         $(window).on("resize", function () {
             floaters.each(function () {
                 var floater = $(this);
-                var spacer = floater.prev();
-                var container = floater.parent();
-                var lastContent = container.find(opts.bottomContent);
-                var floaterHeight = this.offsetHeight;
-                var h = Math.max(0, container.height() - floaterHeight);
-                spacer.height(h);
-                while (h > 0 && floater.offset().top + floaterHeight > lastContent.offset().top + lastContent.height()) {
-                    spacer.height(--h);
-                }
-                while (floater.offset().top + floaterHeight < lastContent.offset().top + lastContent.height()) {
-                    spacer.height(++h);
+                var spacer = floater.prev(".lowFloat-spacer");
+                if (spacer.css("display") == "block") {
+                    var container = floater.parent();
+                    var lastContent = container.find(opts.bottomContent);
+                    var floaterHeight = this.offsetHeight;
+                    var h = Math.max(0, container.height() - floaterHeight);
+                    spacer.height(h);
+                    while (h > 0 && floater.offset().top + floaterHeight > lastContent.offset().top + lastContent.height()) {
+                        spacer.height(--h);
+                    }
+                    while (floater.offset().top + floaterHeight < lastContent.offset().top + lastContent.height()) {
+                        spacer.height(++h);
+                    }
                 }
             });
         }).trigger("resize");
